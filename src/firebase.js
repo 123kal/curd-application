96235989714b6b5d9ed8bff0a7573829c6cf85ca
kalyan <lankireddykalyan087@gmail.com>
2024-04-20 15:10:37 +0530

    firebase

---
+++ src/firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore"; // Import Firestore
+
+const firebaseConfig = {
+  apiKey: "-",
+  authDomain: "-",
+  projectId: "-",
+  storageBucket: "-",
+  messagingSenderId: "-",
+  appId: "-",
+  measurementId: "-"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+console.log("Firebase app initialized successfully!"); // Added console log
+
+const analytics = getAnalytics(app);
+
+export const db = getFirestore(app);
+
+export default app;
